49a81cd6b53ed509d6ed761491f4b97fe3b7209b
Emmanuel Nsikan-David <en56434@gmail.com>
2024-05-24 09:47:42 +0100

    moved to Gemini API

--- server/controllers/MovieController.js
+++ server/controllers/MovieController.js
@@ -1,44 +1,64 @@
-const  OpenAI = require("openai")
 const { unknownError } = require("../actions/errorMessages")
-const openAiApiKey = process.env.OPENAI
 const tmdbApiKey = process.env.TMDB
-const openai = new OpenAI({
-    apiKey: openAiApiKey,
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai")
+
+const apiKey = process.env.GEMINI_API_KEY
+const genAI = new GoogleGenerativeAI(apiKey)
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  systemInstruction: `you are a bot that takes in descriptions of movies by humans and finds the movies that match those description and you will do this with pinpoint accuracy. you will be given a movie description and your job is to return a JSON object containing the status of the request, the names, percentage match and year of release of 7 movies that match that description starting from the one that matches it the most to the one that matches it the least in this format :     
+   { status : //success if any movies were found or failure if no movies were found,
+      data : [{movieName: //name of movie that matches,
+       matchPercent: 90 //an integer representing the accuracy of the match,
+        releaseYear : 2014 //year the movie was released }]
+         //an array of 7 movies that matches the description      
+        }`,
 })
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "application/json",
+};
+
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+];
 
 async function getMoviesThatMatchPrompt(req, res){
     try{
         const {movieDescription} = req.body
-   const movieNames = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-1106",
-      messages: [
-        {
-          role: "system",
-          content: `you are a bot that takes in descriptions of movies by humans and finds the movies that match those description and you will do this with pinpoint accuracy. you will be given a movie description and your job is to return a JSON object containing the status of the request, the names, percentage match and year of release of 7 movies that match that description starting from the one that matches it the most to the one that matches it the least in this format :
-          
-          {
-              status : //success if any movies were found or failure if no movies were found,
-              data : [{
-              movieName: //name of movie that matches,
-              matchPercent: 90 //an integer representing the accuracy of the match,
-              releaseYear : 2014 //year the movie was released
-          }] //an array of 7 movies that matches the description
-      }. `,
-        },
-        {
-          role: "user",
-          content: movieDescription,
-        },
-      ],
-      temperature: 1,
-      max_tokens: 900,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-      response_format: { type: "json_object" }
-    })
+        const chatSession = model.startChat({
+          generationConfig,
+          safetySettings,
+          history: [
+          ],
+        });
+   const movieNames = await chatSession.sendMessage(movieDescription)
 
-    const movieMatches = JSON.parse(movieNames.choices[0].message.content).data.map((match) => {
+    const movieMatches = JSON.parse(movieNames.response.text()).data.map((match) => {
       const movieName = match.movieName;
       return { movieName, matchPercent: match.matchPercent};
     })
@@ -64,6 +84,7 @@ async function getMoviesThatMatchPrompt(req, res){
     res.send(movieInfoArray)
     }
     catch(err){
+      console.log(err)
         res.status(500).json(unknownError)
     }
 }
