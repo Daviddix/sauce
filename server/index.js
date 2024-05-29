02e67e808c147ae21f6083b976a71c3d7cc85060
Emmanuel Nsikan-David <en56434@gmail.com>
2024-05-29 23:54:25 +0100

    fixed title reading undefined error and made textarea sticky

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const listRouter = require("./routes/list")
 
 
 //server configs
-const whitelist = ["http://localhost:5173", "https://deliciouso.netlify.app/", "https://deliciouso.netlify.app"]
+const whitelist = ["http://localhost:5173", "https://sauce-beta.vercel.app/"]
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
