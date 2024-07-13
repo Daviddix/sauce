caf81e7bfebe166e954f85997ea72477cbc2b1f0
Emmanuel Nsikan-David <en56434@gmail.com>
2024-07-13 15:40:31 +0100

    added attribution

--- client/src/WebApp/Pages/Home/Home.jsx
+++ client/src/WebApp/Pages/Home/Home.jsx
@@ -83,8 +83,6 @@ function Home() {
         
     </section>
 
-
-
     <ChatInput  />
     </>
   )
--- client/src/WebApp/components/WelcomeMessage/WelcomeMessage.jsx
+++ client/src/WebApp/components/WelcomeMessage/WelcomeMessage.jsx
@@ -6,7 +6,11 @@ function WelcomeMessage() {
     <div className="welcome">
                     <img src={logo} alt="sauce logo" className="sauce-logo" />
                     <h1 className="tight-heading-style">Sauce</h1>
-                    <p className="sub-body-style">Hi and welcome to Sauce! Think of me as your friendly movie companion. Can't quite remember a movie? Share some basic details and i'll find your forgotten movie 😄</p>
+                    <p className="sub-body-style">
+                    Hello and welcome to Sauce! I'm here to be your helpful companion for movies, anime, or TV shows. If you can't quite recall a specific one, just share some details and I'll track down your forgotten favorite! 😄</p>
+
+                    <span className="sub-body-style">Powered by <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_1-8ba2ac31f354005783fab473602c34c3f4fd207150182061e425d366e4f34596.svg" alt="tmdb logo" />
+                    </span>
                 </div>
   )
 }
