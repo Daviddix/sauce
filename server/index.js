e76f5e9d090cd135bb34552915b6861d86178dfc
Emmanuel Nsikan-David <en56434@gmail.com>
2024-07-25 11:03:08 +0100

    cors update

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ const corsOptions = {
 } 
 
 //middlewares
-app.use(cors()) 
+app.use(cors(corsOptions)) 
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({limit: '50mb'}))
 app.use(cookieParser())
